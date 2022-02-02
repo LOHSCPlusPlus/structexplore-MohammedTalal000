6d9e13678d92e4898a649fc5ab23ee8152cf07aa
MohammedTALAL1 <>
2022-02-02 22:26:18 +0000

    Practice5 working

---
+++ Practice5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// Put the person struct here
+  struct PersonName {
+    string firstName;
+    string lastName;
+    PersonName();
+    PersonName(string first, string last);
+  };
+
+  PersonName::PersonName() {
+    firstName = "Unknown";
+    lastName = "Unknown";
+  }
+  PersonName::PersonName(string first, string last) {
+    firstName = first;
+    lastName = last;
+  }
+
+
+void printPerson(PersonName person) {
+
+cout << "First name: " << person.firstName << endl << "last Name: " << person.lastName << endl << endl;
+
+}
+
+int main() {
+	// Create two instances of person here, and assign the names.
+  PersonName person1("Linus", "Van Pelt");
+  printPerson(person1);
+
+  PersonName person2;
+  printPerson(person2);
+
+	// Add the prints here
+
+	return 0;
+}
